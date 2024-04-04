c44fa1f50893c99f931f21db01ddbf114914dc30
imarun0001 <arunanshu.patra68@gmail.com>
2024-04-04 23:57:26 +0530

    Add existing file

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')   #added later for deployment by ap
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # emailable settings
