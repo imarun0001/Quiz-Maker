d5c74170cc62dde9106d2eb1e13f38e05bc7ddcb
imarun0001 <arunanshu.patra68@gmail.com>
2024-04-05 12:54:13 +0530

    Add existing file

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,10 @@
 echo " BUILD START"
 #python3.7 pip install Django==4.2.7
-python3.7 -m pip install -r requirements.txt
+
 #echo "Make Migration..."
 #python3.9 manage.py makemigrations --noinput
 #python3.9 manage.py migrate --noinput
-python3.7 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
 echo " BUILD END"
 
