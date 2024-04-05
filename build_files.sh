36a61dfb1649e2411a261068187faa02d469bb24
imarun0001 <arunanshu.patra68@gmail.com>
2024-04-06 00:17:19 +0530

    Add existing file

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # build_files.sh
 echo " BUILD START"
-pip install -r requirements.txt
+pipensure install -r requirements.txt
 echo "Make Migration..."
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
