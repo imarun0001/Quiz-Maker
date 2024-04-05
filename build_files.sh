265a5d7e08d5a238a1b91a054e51a4c354e43436
imarun0001 <arunanshu.patra68@gmail.com>
2024-04-05 15:57:23 +0530

    Add existing file

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo " BUILD START"
-pip uninstall django
-python3 -m pip install -r requirements.txt
+python3 -m pip3 install -r requirements.txt
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
