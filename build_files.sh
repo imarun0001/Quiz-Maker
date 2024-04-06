f61757a62b8fa6ee1616d4115fba3f5b6a220cb9
imarun0001 <arunanshu.patra68@gmail.com>
2024-04-06 18:13:44 +0530

    Add existing file

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 echo " BUILD START"
-cd venv-django
-.\Scripts\activate
-cd..
-python3.10 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 echo "Make Migration..."
 python3.10 manage.py makemigrations --noinput
 python3.10 manage.py migrate --noinput
